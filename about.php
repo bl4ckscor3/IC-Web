e20e093c400004e4c93ff6db561b28b5f62e84ff
bl4ckscor3 <donalddani9612@gmx.de>
2016-02-27 21:54:15 +0100

    about page

--- about.php
+++ about.php
@@ -6,51 +6,52 @@
 		<?php require('header.php')?>
 	    <div class="container">
 	        <div class="row">
-	            <div class="box">
-	                <div class="col-lg-12">
-	                    <hr>
-	                    <h2 class="intro-text text-center">About
-	                        <strong>business casual</strong>
-	                    </h2>
-	                    <hr>
-	                </div>
-	                <div class="col-md-6">
-	                    <img class="img-responsive img-border-left" src="res/slide-2.jpg" alt="">
-	                </div>
-	                <div class="col-md-6">
-	                    <p>This is a great place to introduce your company or project and describe what you do.</p>
-	                    <p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.</p>
-	                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-	                </div>
-	                <div class="clearfix"></div>
+	            <div class="box text-center">
+                    <hr>
+                    <h2 class="intro-text text-center">About <strong>IgneousCraft</strong></h2>
+                    <hr>
 	            </div>
 	        </div>
 	        <div class="row">
 	            <div class="box">
 	                <div class="col-lg-12">
 	                    <hr>
-	                    <h2 class="intro-text text-center">Our
-	                        <strong>Team</strong>
-	                    </h2>
+	                    <h2 class="intro-text text-center">Our <strong>Team</strong></h2>
 	                    <hr>
 	                </div>
 	                <div class="col-sm-4 text-center">
-	                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-	                    <h3>John Smith
-	                        <small>Job Title</small>
+	                    <h3>Vauff<br>
+	                        <small>Server Owner</small><br>
+	                        <small>Developer</small>
 	                    </h3>
 	                </div>
 	                <div class="col-sm-4 text-center">
-	                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-	                    <h3>John Smith
-	                        <small>Job Title</small>
+	                    <h3>bl4ckscor3<br>
+	                        <small>Co-Owner</small><br>
+	                        <small>Developer</small>
 	                    </h3>
 	                </div>
 	                <div class="col-sm-4 text-center">
-	                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-	                    <h3>John Smith
-	                        <small>Job Title</small>
+	                    <h3>Vakonof<br>
+	                        <small>Admin</small><br>
+	                        <small>Build Team</small>
 	                    </h3>
+	                </div>
+	                <br>
+	                <div class="col-sm-4 text-center">
+	                	<h3>JustRamon<br>
+	                		<small>Moderator</small><br>
+	                		<small>Developer</small>
+	                	</h3>
+	                </div>
+	                <div style="padding-top: 25px" class="col-sm-4 text-center">
+	                	<button class="btn btn-primary btn-lg" onclick="location.href=''">Apply for<br>Staff here</button>
+	                </div>
+	                <div class="col-sm-4 text-center">
+	                	<h3>ExcelGamer<br>
+	                		<small>Moderator</small><br>
+	                		<small>Build Team</small>
+	                	</h3>
 	                </div>
 	            </div>
 	        </div>
