2ec5e9caaeb62b6dc6640ca5635b26239e4b6d1d
bl4ckscor3 <donalddani9612@gmx.de>
2016-02-27 01:06:55 +0100

    edited template to fit ic needs

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php $title = "Contact us";
+	require('htmlHead.php')?>
+	<body>
+		<?php require('header.php')?>
+	    <div class="container">
+	        <div class="row">
+	            <div class="box">
+	                <div class="col-lg-12">
+	                    <hr>
+	                    <h2 class="intro-text text-center">Contact <strong>business casual</strong>
+	                    </h2>
+	                    <hr>
+	                </div>
+	                <div class="col-md-4">
+	                    <p>Phone:
+	                        <strong>123.456.7890</strong>
+	                    </p>
+	                    <p>Email:
+	                        <strong><a href="mailto:name@example.com">name@example.com</a></strong>
+	                    </p>
+	                    <p>Address:
+	                        <strong>3481 Melrose Place
+	                            <br>Beverly Hills, CA 90210</strong>
+	                    </p>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	    <?php require('footer.php')?>
+	</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<p>Copyright &copy; IgneousCraft <?php echo date("Y", time());?></p>
+			</div>
+		</div>
+	</div>
+</footer>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
