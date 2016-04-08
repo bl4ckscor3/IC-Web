2de95b9c52d32f4d1d6586aa59aa0d66d5e47fb5
bl4ckscor3 <donalddani9612@gmx.de>
2016-04-08 12:32:47 +0200

    added blackhawk's page

--- IC-Website/blackhawk8100.php
+++ IC-Website/blackhawk8100.php
@@ -11,8 +11,10 @@
                     <h2 class="intro-text text-center">About <strong>BlackHawk8100</strong></h2>
                     <hr>
                     <img src="res/blackhawk8100.png" width="150px" height="150px"><br><br>
-                    <div class="intro-text-normal"></div>
-	            </div>
+                    <div class="intro-text-normal">
+                    	<small>Hello and welcome to our humble server, IgneousCraft!  I am BlackHawk8100, a builder for the server, and I am a kinda goofy chill person.  If you see me on IRC or on the server and need anything, let me know and I'd love to help you out!</small>
+                    </div><br><br>
+	            </div><br><br><br>
 	        </div>
 	    </div>
 		<?php require('footer.php')?>
